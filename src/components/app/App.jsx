379b3dde03d432bc10a52900fa876ffd438d5fd5
Anthony Rosario <anthonymrosario225@gmail.com>
2021-05-19 11:45:06 -0700

    laid out boilerplate file structure and created services function

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,20 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route 
+          exact="/"
+          component={}
+        />
+        <Route 
+          exact="/:id"
+          component={}
+        />
+      </Switch>
+    </Router>
+  )
 }
---
+++ src/services/heyarnoldsApi.js
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+export async function getCharacters() {
+  const response = await fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters');
+  const { json } = await response.json();
+
+  return json;
+
+}
+
+export async function getCharacterById(id) {
+  const response = await fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/${id}`);
+  const json = await response.json();
+
+  return json;
+}
